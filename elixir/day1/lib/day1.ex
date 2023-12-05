7f0639c2f2052ba0939a2fd1c5f2276ac970f4bb
Henry Maddocks <hmaddocks@me.com>
2023-12-05 21:56:32 +1300

    Elixir day 1 part 1

---
+++ elixir/day1/lib/day1.ex
@@ -0,0 +1,38 @@
+defmodule Day1 do
+  def find_digit({head, tail}) do
+    case Integer.parse(head) do
+      {num, _} -> num
+      _ -> find_digit(String.next_grapheme(tail))
+    end
+  end
+
+  def find_digit(nil) do
+    0
+  end
+
+  def process_line(line) do
+    first =
+      line
+      |> String.next_grapheme()
+      |> find_digit()
+
+    last =
+      line
+      |> String.reverse(line)
+      |> String.next_grapheme()
+      |> find_digit()
+
+    first * 10 + last
+  end
+
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&process_line/1)
+    |> Enum.sum()
+  end
+end
+
+File.read!(~c"input.txt")
+|> Day1.part1()
+|> IO.inspect(label: "part1")
