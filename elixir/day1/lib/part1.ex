33ab7f2cf379d4f15c3490bad0723748d8473704
Henry Maddocks <hmaddocks@me.com>
2023-12-07 19:50:53 +1300

    Refactor Elixir day 1 part 1

--- elixir/day1/lib/part1.ex
+++ elixir/day1/lib/part1.ex
@@ -1,34 +1,19 @@
 defmodule Part1 do
-  def find_digit({head, tail}) do
-    case Integer.parse(head) do
-      {num, _} -> num
-      _ -> find_digit(String.next_grapheme(tail))
-    end
-  end
-
-  def find_digit(nil) do
-    0
-  end
-
-  def process_line(line) do
-    first =
-      line
-      |> String.next_grapheme()
-      |> find_digit()
-
-    last =
-      line
-      |> String.reverse()
-      |> String.next_grapheme()
-      |> find_digit()
-
-    first * 10 + last
-  end
-
   def part1(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&process_line/1)
+    |> Enum.map(fn line ->
+      line
+      |> String.graphemes()
+      |> Enum.filter(fn char ->
+        if String.match?(char, ~r/\d/) do
+          char
+        end
+      end)
+    end)
+    |> Enum.map(fn numbers ->
+      String.to_integer(Enum.at(numbers, 0)) * 10 + String.to_integer(Enum.at(numbers, -1))
+    end)
     |> Enum.sum()
   end
 end
--- elixir/day1/test/part1_test.exs
+++ elixir/day1/test/part1_test.exs
@@ -1,19 +1,6 @@
 defmodule Part1Test do
   use ExUnit.Case
 
-  test "find_digit/1 returns the first digit in a list" do
-    assert Part1.find_digit(String.next_grapheme("123abc")) == 1
-    assert Part1.find_digit(String.next_grapheme("abc123")) == 1
-    assert Part1.find_digit(String.next_grapheme("abc")) == 0
-    assert Part1.find_digit(String.next_grapheme("")) == 0
-  end
-
-  test "process_line/1 returns the first and last digit in a line" do
-    assert Part1.process_line("123abc") == 13
-    assert Part1.process_line("abc123") == 13
-    assert Part1.process_line("abc") == 0
-  end
-
   test "part1" do
     input = """
     1abc2
