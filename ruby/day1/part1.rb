5aef7d81b80f42fe3b17fbc10fe97d9c5816677f
Henry Maddocks <hmaddocks@me.com>
2023-12-07 19:49:54 +1300

    Refactor Ruby day 1 part 1

--- ruby/day1/part1.rb
+++ ruby/day1/part1.rb
@@ -1,23 +1,9 @@
 # frozen_string_literal: true
 
-def find_digit(line)
-  line.each_char { |char| return char.to_i if char.match?(/\d/) }
-  nil
-end
-
-def process_line(line)
-  first = find_digit(line)
-  last = find_digit(line.reverse)
-
-  if first && last
-    (first * 10) + last
-  else
-    0
-  end
-end
-
 def part1(input)
-  input.split("\n").sum { |line| process_line(line) }
+  input.split("\n")
+       .filter_map { |line| line.chars.filter_map { |char| char if char.match?(/\d/) } }
+       .sum { |numbers| (numbers.first.to_i * 10) + numbers.last.to_i }
 end
 
 if __FILE__ == $PROGRAM_NAME
--- ruby/day1/part1_spec.rb
+++ ruby/day1/part1_spec.rb
@@ -2,28 +2,6 @@
 
 require_relative 'part1'
 
-describe '#process_line' do
-  it 'returns 0 when the line is empty' do
-    expect(process_line('')).to eq(0)
-  end
-
-  it 'returns 0 when the line has no digits' do
-    expect(process_line('abcdef')).to eq(0)
-  end
-
-  it 'returns the correct value when the line has one digit' do
-    expect(process_line('a1b')).to eq(11)
-  end
-
-  it 'returns the correct value when the line has two digits' do
-    expect(process_line('a1b2c')).to eq(12)
-  end
-
-  it 'returns the correct value when the line has more than two digits' do
-    expect(process_line('a1b2c3d4')).to eq(14)
-  end
-end
-
 describe '#part1' do
   it "returns 142 when given the example input" do
     input = <<~INPUT
