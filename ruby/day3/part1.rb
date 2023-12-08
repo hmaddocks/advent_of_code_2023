08a023875d6205fff89897f11edccaf9ea3c32cd
Henry Maddocks <hmaddocks@me.com>
2023-12-08 17:06:21 +1300

    Ruby day 3 part 1

--- ruby/day3/part1.rb
+++ ruby/day3/part1.rb
@@ -1,6 +1,50 @@
 # frozen_string_literal: true
+
+def find_symbols(input)
+  [].tap do |symbols|
+    input.each_char.with_index do |char, index|
+      symbols << index if char.match?(/[^\d.]/)
+    end
+  end
+end
+
+def find_numbers(input)
+  str = input
+  list = []
+  cursor = 0
+
+  loop do
+    m = str.match(/\d+/)
+    break if m.nil?
+
+    number = m[0]
+    offset = m.offset(0)[0] + cursor
+    list << [number, offset, offset + number.length - 1]
+    str = m.post_match
+    cursor += m.offset(0)[1]
+  end
+  list
+end
+
+def adjacent?(symbol_pos, number, width, positions)
+  return false if symbol_pos < number[1] - (width + 1) || symbol_pos > (number[2] + width + 1)
+
+  positions.any? do |position|
+    (symbol_pos + position) >= number[1] && (symbol_pos + position) <= number[2]
+  end
+end
+
 def part1(input)
-  0
+  width = input.split("\n").first.length
+  input = input.delete("\n")
+
+  positions = [-(width + 1), -width, -(width - 1), -1, 1, width - 1, width, width + 1]
+  symbols = find_symbols(input)
+  numbers = find_numbers(input)
+
+  symbols.flat_map { |symbol| numbers.select { |number| adjacent?(symbol, number, width, positions) } }
+         .uniq
+         .reduce(0) { |sum, number| sum + number.first.to_i }
 end
 
 if __FILE__ == $PROGRAM_NAME
--- ruby/day3/part1_spec.rb
+++ ruby/day3/part1_spec.rb
@@ -2,21 +2,76 @@
 
 require_relative 'part1'
 
-describe '#part1' do
-  it "verks" do
-    input = <<~INPUT
-      467..114..
-      ...*......
-      ..35..633.
-      ......#...
-      617*......
-      .....+.58.
-      ..592.....
-      ......755.
-      ...$.*....
-      .664.598..
-    INPUT
+describe "Part1" do
+  describe "#find_symbols" do
+    it "finds the symbols" do
+      input = "467..114.....*........35..633.......#...617*...........+.58...592...........755....$.*.....664.598.."
+      expect(find_symbols(input)).to eq([13, 36, 43, 55, 83, 85])
+    end
+  end
 
-    expect(part1(input)).to eq(4361)
+  describe "#find_numbers" do
+    it "finds the numbers" do
+      input = "467..114.....*........35..633.......#...617*...........+.58...592...........755....$.*.....664.598.."
+      expect(find_numbers(input)).to match_array(
+        [
+          ["467", 0, 2],
+          ["114", 5, 7],
+          ["35", 22, 23],
+          ["633", 26, 28],
+          ["617", 40, 42],
+          ["58", 57, 58],
+          ["592", 62, 64],
+          ["755", 76, 78],
+          ["664", 91, 93],
+          ["598", 95, 97]
+        ]
+      )
+    end
+  end
+
+  describe "#adjacent?" do
+    it "returns true when symbol is adjacent to the number" do
+      number = ["633", 26, 28]
+      width = 10
+      positions = [-(width + 1), -width, -(width - 1), -1, 1, width - 1, width, width + 1]
+      symbol = 36
+      expect(adjacent?(symbol, number, width, positions)).to eq(true)
+    end
+
+    it "returns false when symbol is not adjacent to the number" do
+      number = ["58", 57, 58]
+      width = 10
+      positions = [-(width + 1), -width, -(width - 1), -1, 1, width - 1, width, width + 1]
+      symbol = 55
+      expect(adjacent?(symbol, number, width, positions)).to eq(false)
+    end
+
+    it "returns false when symbol is not near to the number" do
+      number = ["633", 26, 28]
+      width = 10
+      positions = [-(width + 1), -width, -(width - 1), -1, 1, width - 1, width, width + 1]
+      symbol = 13
+      expect(adjacent?(symbol, number, width, positions)).to eq(false)
+    end
+end
+
+  describe '#part1' do
+    it "verks" do
+      input = <<~INPUT
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+      INPUT
+
+      expect(part1(input)).to eq(4361)
+    end
   end
 end
