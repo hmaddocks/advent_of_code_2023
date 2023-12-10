451b1543279ac5553a6d0b45d74aa8abf854fc80
Henry Maddocks <hmaddocks@me.com>
2023-12-10 19:50:53 +1300

    Elixir day 2 part 1

--- elixir/day2/lib/part1.ex
+++ elixir/day2/lib/part1.ex
@@ -1,5 +1,54 @@
 defmodule Part1 do
-  def hello do
-    :world
+  def parse_line(line) do
+    line
+    |> String.split(";")
+    |> Enum.map(fn game ->
+      game
+      |> String.split(",")
+      |> Enum.reduce(%{}, fn bag, acc ->
+        [count, colour] = String.split(String.trim(bag))
+        Map.put(acc, String.to_atom(colour), String.to_integer(count))
+      end)
+    end)
+  end
+
+  def parse_game(game) do
+    bag = %{
+      red: 12,
+      green: 13,
+      blue: 14
+    }
+
+    game
+    |> Enum.all?(fn turn ->
+      reds = turn[:red] || 0
+      blues = turn[:blue] || 0
+      greens = turn[:green] || 0
+
+      reds <= bag[:red] && blues <= bag[:blue] && greens <= bag[:green]
+    end)
+  end
+
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn line, acc ->
+      [_, game_id, game] = Regex.run(~r/Game (\d+): (.*)/, line)
+      turns = parse_line(game)
+
+      if parse_game(turns) do
+        acc +
+          case Integer.parse(game_id) do
+            {i, _} -> i
+            _ -> 0
+          end
+      else
+        acc
+      end
+    end)
   end
 end
+
+File.read!("input.txt")
+|> Part1.part1()
+|> IO.inspect(label: "part1")
