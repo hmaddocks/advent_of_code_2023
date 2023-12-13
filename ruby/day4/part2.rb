a150f76f34156bbb1d2fe83c0cd60953a51492b0
Henry Maddocks <hmaddocks@me.com>
2023-12-13 22:26:36 +1300

    Refactor Ruby day 4 part 2

--- ruby/day4/part2.rb
+++ ruby/day4/part2.rb
@@ -1,22 +1,20 @@
+# frozen_string_literal: true
+
 Game = Struct.new(:wins, :count)
 
-def parse(game)
-  game.split("|")
-end
-
 def part2(input)
-  lines = input.split("\n")
-
-  games = lines
-          .map { |line| line.split(":").last }
-          .map { |game| parse(game) }
-          .map { |(left, right)| Game.new(left.split.map(&:to_i).intersection(right.split.map(&:to_i)).count, 1) }
+  games = input.split("\n")
+               .map { |line| line.split(":").last.split("|") }
+               .map do |(left, right)|
+                 Game.new((left.split.map(&:to_i) & right.split.map(&:to_i)).count, 1)
+               end
 
   games.each.with_index do |game, i|
     (i + 1).upto(i + game.wins) do |j|
       games[j].count += game.count if games[j]
     end
   end
+
   games.sum(&:count)
 end
 
